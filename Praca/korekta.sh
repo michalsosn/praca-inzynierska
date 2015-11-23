bd56c56a879a4bed6a23e9ad16267a51384ae36b
Michał Sośnicki <sosnicki.michal@gmail.com>
2015-11-23 14:18:02 +0100

    Szablon pracy inżynierskiej i przykładowa prezentacja w latex2thesis.

---
+++ Praca/korekta.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+sed -i -e 's/ w / w~/g' *.tex
+sed -i -e 's/ i / i~/g' *.tex
+sed -i -e 's/ z / z~/g' *.tex
+sed -i -e 's/ o / o~/g' *.tex
+sed -i -e 's/ a / a~/g' *.tex
+sed -i -e 's/ W / W~/g' *.tex
+sed -i -e 's/ Z / Z~/g' *.tex
+sed -i -e 's/ O / O~/g' *.tex
+sed -i -e 's/ A / A~/g' *.tex
